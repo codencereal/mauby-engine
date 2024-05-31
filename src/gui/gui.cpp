8f1693e135df64e41626f7fbe927ca25a32c9ee3
codencereal <47301026+codencereal@users.noreply.github.com>
2024-05-31 12:29:56 -0400

    Clean up improper indentation in gui.cpp

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -5,36 +5,34 @@
 #include "GLFW/glfw3.h"
 
 extern "C" {
+  void init_gui(GLFWwindow* window) {
+      IMGUI_CHECKVERSION();
+      ImGui::CreateContext();
+      ImGuiIO& io = ImGui::GetIO();
+      io.IniFilename = NULL; // No ini file
+      io.LogFilename = NULL; // No log file
+      ImGui::StyleColorsDark();
+      ImGui_ImplGlfw_InitForOpenGL(window, true);
+      ImGui_ImplOpenGL3_Init("#version 130");
+  }
 
-void init_gui(GLFWwindow* window) {
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.IniFilename = NULL; // No ini file
-    io.LogFilename = NULL; // No log file
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
-}
+  void render_gui() {
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
 
-void render_gui() {
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+      // Example window
+      ImGui::Begin("Hello, world!");
+      ImGui::Text("This is a GUI test!");
+      ImGui::End();
 
-    // Example window
-    ImGui::Begin("Hello, world!");
-    ImGui::Text("This is a GUI test!");
-    ImGui::End();
+      ImGui::Render();
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  }
 
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void shutdown_gui() {
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-}
-
+  void shutdown_gui() {
+      ImGui_ImplOpenGL3_Shutdown();
+      ImGui_ImplGlfw_Shutdown();
+      ImGui::DestroyContext();
+  }
 }
